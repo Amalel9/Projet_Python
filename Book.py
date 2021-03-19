31bfdae1388e3eb782bcfabc293dd3ca1987de1e
Amal EL JABRI <amal.eljabri@hotmail.com>
2021-03-19 15:12:19 +0100

    Classe Order : method Lt, DisplayOrder1, DisplayOrder2

--- Book.py
+++ Book.py
@@ -21,3 +21,28 @@ class Order:
         if self.price == other.price :
             res = True
         return res
+
+        ''' This method allow to compare 2 Order objects.
+    One Order1 is lower than an Order2 if the price of Order1 is lower than the price of the 
+    Order2.'''
+
+    def __lt__(self,other):
+        res = False
+        if self.price < other.price : 
+            res = True
+        return res
+
+    ''' Those 2 method allow to display the object Order. The difference is only for good looking when
+    we need to display some result.'''
+
+    def display_order1(self):
+        if self.side == "SELL":
+            if self.quantity !=0 : 
+               print(self.side, "",self.quantity ,"@",self.price," ","id = ",self.iD)
+               #print (self.quantity) test
+        elif self.quantity != 0 :
+            print(self.side, " ",self.quantity,"@",self.price," ","id = ",self.iD)
+
+    def display_order2(self):
+        print(self.side, " ",self.quantity,"@",self.price," ","id = ",self.iD, end="")
+        
